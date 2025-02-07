c6932f1f29486874290f874ef5d7754ecca693bf
Bruna Campos <brunareginadecampos@gmail.com>
2025-02-06 21:58:15 -0300

    segunda parte pronta

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Jogo de adivinhaÃ§Ã£o
+# Jogo de adivinhação
 Lingagem C
 
 gcc programa.c -o programa.exe
+
+
+#include <locale.h>
+// permite colocar acentos
+    setlocale(LC_ALL, "Portuguese");
+    
+    trocar utf8 por iso8859-1
--- adivinhacao.c
+++ adivinhacao.c
@@ -1,9 +1,38 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main () {
+    // permite colocar acentos
+    setlocale(LC_ALL, "Portuguese");
+
+
     // escrever na tela
     // imprime cabecalho do nosso jogo
-    printf ("****************************************\n");
-    printf ("* Bem vindo ao nosso jogo de adivinhacao *\n");
-    printf ("****************************************\n");
+    printf("****************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("****************************************\n");
+
+     int numerosecreto = 42;
+
+     int chute;
+
+     printf("Qual é o seu chute? ");
+     scanf("%d", &chute); 
+     printf("Seu chute foi %d\n", chute);
+
+    //Se o chute for igual ao numero secreto
+    if(chute == numerosecreto) {
+     printf("Parabéns! Você acertou!!\n");
+     printf("Jogue de novo, você é um bom jogador!\n");
+     } else {
+        //Caso contrário
+        if(chute > numerosecreto) {
+            printf("Seu chute foi maior que o númeo secreto!");
+        }
+
+        if(chute < numerosecreto) {
+            printf("Seu chute foi menor que o número secreto!");
+        }
+
+     }
 }
