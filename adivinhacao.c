63768808989a8a43998520ff898fa505702be26e
Bruna Campos <brunareginadecampos@gmail.com>
2025-02-10 22:10:59 -0300

    aula 2 finalizadao

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main () {
+int main() {
     // permite colocar acentos
     setlocale(LC_ALL, "Portuguese");
-
 
     // escrever na tela
     // imprime cabecalho do nosso jogo
@@ -12,27 +11,44 @@ int main () {
     printf("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf("****************************************\n");
 
-     int numerosecreto = 42;
+    int numerosecreto = 42;
 
-     int chute;
+    int chute;
+    int tentativas = 1;
 
-     printf("Qual é o seu chute? ");
-     scanf("%d", &chute); 
-     printf("Seu chute foi %d\n", chute);
+    //Enquanto o jogador não ganhou
+    while(1) {
+        printf("Tentativa %d\n", tentativas);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
 
-    int acertou = (chute == numerosecreto);
+        if(chute < 0) {
+            printf("Você não pode chutar números negativos!\n");
+            continue;
+        }
 
-    //Se o chute for igual ao numero secreto
-    if(acertou) {
-     printf("Parabéns! Você acertou!!\n");
-     printf("Jogue de novo, você é um bom jogador!\n");
-     } else {
+        int acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
-        //Caso contrário
-        if(maior) {
+
+        // Se o chute for igual ao numero secreto
+        if (acertou){
+            printf("Parabéns! Você acertou!!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+
+            break;
+        }
+        else if (maior) {
+            // Caso contrário
             printf("Seu chute foi maior que o númeo secreto!\n");
-        } else {
+        }
+        else{
             printf("Seu chute foi menor que o número secreto!\n");
         }
-     }
+
+        tentativas++;
+    }
+
+    printf("Fim de jogo!\n");
+    printf("Você acertou em %d tentativas!", tentativas);
 }
